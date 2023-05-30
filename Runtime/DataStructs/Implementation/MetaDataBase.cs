236f587243e414eb02bbbf34ca519335f54f1322
LordJEMO <jemo666z@gmail.com>
2023-05-30 18:59:25 +0200

    Modify Utilities and GuadClauses namespace
    Modify Utilities and GuadClauses namespace

---
+++ Runtime/DataStructs/Implementation/MetaDataBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace DTTUnityCommon.DataStructs
+{
+    [Serializable]
+    public class MetaDataBase : IMetaData
+    {
+        [SerializeField] private string _id;
+
+        public MetaDataBase()
+        {
+            _id = Guid.NewGuid().ToString();
+        }
+
+        public MetaDataBase(string id)
+        {
+            _id = id;
+        }
+
+        public string Id { get => _id;  }
+    }
+
+    [Serializable]
+    public class MetaDataNumber : MetaDataBase
+    {
+        [SerializeField]
+        private int _number;
+
+        public MetaDataNumber(int number) : base(Guid.NewGuid().ToString())
+        {
+            _number = number;
+        }
+
+        public MetaDataNumber(string id, int number) : base(id)
+        {
+            _number = number;
+        }
+
+        public int Number { get => _number; set => _number = value; }
+    }
+}
+
---
+++ Runtime/Utilities/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DTTUnityCommon
+{
+    public class Utilities
+    {
+        public static void SafeDestroy(GameObject gameObject)
+        { 
+            if(Application.isEditor) 
+            {
+                Object.DestroyImmediate(gameObject);
+            }
+            else
+            {
+                Object.Destroy(gameObject);
+            }
+        }
+    }
+}
